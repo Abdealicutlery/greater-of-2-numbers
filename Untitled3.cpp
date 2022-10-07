8db414d474443508496e765e197ebac8ca7a4453
Abdealicutlery <114421001+Abdealicutlery@users.noreply.github.com>
2022-10-07 14:25:27 +0530

    Add files via upload

---
+++ Untitled3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ using namespace std;
+ int main ()
+ {
+     float qty,price,total,discount;
+     cout<<"enter qty and price";
+     cin>>qty>>price;
+     total=qty*price;
+     if (total>1000)
+     {
+         discount=0.1*total;
+         cout<<"discount amount:"<<discount<<endl;
+         total=total-discount;
+
+     }
+     cout<<"amount to be paid"<<total;
+     return 0;
+      }
